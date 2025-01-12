142eeb9003399b6451c1868ccbf0a95c72190a83
Joao Victor <devjoaovictor10@gmail.com>
2025-01-12 17:02:02 -0300

    feat: funcion getTimeList

--- app/barbershops/[id]/_helpes/hours.ts
+++ app/barbershops/[id]/_helpes/hours.ts
@@ -1,19 +1,53 @@
-import { ptBR } from "date-fns/locale";
-import { addMinutes, format, setHours, setMinutes } from "date-fns";
+import { Booking } from "@prisma/client";
+import { isPast, isToday, set } from "date-fns";
 
-export function generateDayTimeList(date: Date): string[] {
-  const startTime = setMinutes(setHours(date, 9), 0);
-  const endTime = setMinutes(setHours(date, 21), 0);
+const TIME_LIST = [
+  "08:00",
+  "08:30",
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+];
 
-  const interval = 45;
-  const timeList: string[] = [];
-
-  let currentTime = startTime;
-
-  while (currentTime <= endTime) {
-    timeList.push(format(currentTime, "HH:mm", { locale: ptBR }));
-    currentTime = addMinutes(currentTime, interval);
-  }
-
-  return timeList;
+interface GetTimeListProps {
+  bookings: Booking[];
+  selectedDay: Date;
 }
+
+export const getTimeList = ({ bookings, selectedDay }: GetTimeListProps) => {
+  return TIME_LIST.filter((time) => {
+    const hour = Number(time.split(":")[0]);
+    const minutes = Number(time.split(":")[1]);
+
+    const timeIsOnThePast = isPast(set(new Date(), { hours: hour, minutes }));
+    if (timeIsOnThePast && isToday(selectedDay)) {
+      return false;
+    }
+
+    const hasBookingOnCurrentTime = bookings.some(
+      (booking) =>
+        booking.date.getHours() === hour &&
+        booking.date.getMinutes() === minutes,
+    );
+    if (hasBookingOnCurrentTime) {
+      return false;
+    }
+    return true;
+  });
+};
