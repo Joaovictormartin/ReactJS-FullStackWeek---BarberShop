c4ae0624068bfa25ac4dcb7d850c07330ff14220
Joao Victor <devjoaovictor10@gmail.com>
2025-01-12 18:56:05 -0300

    fix: ajuste page BarberPage

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -1,47 +1,56 @@
-import { db } from "@/app/_lib/prisma";
-import { redirect } from "next/navigation";
+import { db } from "@/_lib/prisma";
+import Header from "@/_components/header";
+import Search from "@/(home)/_components/search";
+import BarberShopItem from "@/(home)/_components/barbershop-item";
 
-import Header from "@/app/_components/header";
-import Search from "@/app/(home)/_components/search";
-import BarberShopItem from "@/app/(home)/_components/barbershop-item";
-
-interface BarberShopsPageProps {
-  searchParams: { search?: string };
+interface BarbershopsPageProps {
+  searchParams: {
+    title?: string;
+    service?: string;
+  };
 }
 
-const BarberShopsPage = async ({ searchParams }: BarberShopsPageProps) => {
-  if (!searchParams.search) redirect("/");
-
+const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
   const barbershops = await db.barbershop.findMany({
     where: {
-      name: {
-        contains: searchParams.search,
-        mode: "insensitive",
-      },
+      OR: [
+        searchParams?.title
+          ? { name: { contains: searchParams?.title, mode: "insensitive" } }
+          : {},
+        searchParams.service
+          ? {
+              services: {
+                some: {
+                  name: { contains: searchParams.service, mode: "insensitive" },
+                },
+              },
+            }
+          : {},
+      ],
     },
   });
 
   return (
-    <>
+    <div>
       <Header />
+      <div className="my-6 px-5">
+        <Search />
+      </div>
 
-      <div className="px-5 py-6">
-        <Search defaultValues={{ search: searchParams.search }} />
+      <div className="px-5">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Resultados para &quot;{searchParams?.title || searchParams?.service}
+          &quot;
+        </h2>
 
-        <h1 className="mt-5 text-xs font-bold uppercase text-gray-400">
-          Resultados para "{searchParams.search}"
-        </h1>
-
-        <div className="mt-3 grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           {barbershops.map((barbershop) => (
-            <div key={barbershop.id} className="w-full">
-              <BarberShopItem barbershop={barbershop} />
-            </div>
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default BarberShopsPage;
+export default BarbershopsPage;
