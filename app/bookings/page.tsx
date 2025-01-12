3ae82944b1fad3dd032bc5f0dcf45b61601fdb0a
Joao Victor <devjoaovictor10@gmail.com>
2025-01-12 18:57:48 -0300

    fix: ajuste na page Bookings

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,69 +1,69 @@
-import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 
-import { db } from "@/app/_lib/prisma";
-import Header from "@/app/_components/header";
-import { authOptions } from "@/app/_lib/auth";
-import BookingItem from "@/app/_components/booking-item";
+import { authOptions } from "@/_lib/auth";
+import Header from "@/_components/header";
+import BookingItem from "@/_components/booking-item";
+import SignInDialog from "@/_components/sign-in-dialog";
+import { getConfirmedBookings } from "@/_actions/get-confirmed-bookings";
+import { getConcludedBookings } from "@/_actions/get-concluded-bookings";
+import { Dialog } from "@/_components/ui/dialog";
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions);
 
-  if (!session?.user) redirect("/");
+  if (!session?.user) {
+    return (
+      <div className="mt-20 flex flex-col items-center justify-center">
+        <Dialog>
+          <SignInDialog />
+        </Dialog>
+      </div>
+    );
+  }
 
-  const [confirmedBookings, finishedBookings] = await Promise.all([
-    db.booking.findMany({
-      where: {
-        userId: (session.user as any).id,
-        date: { gte: new Date() },
-      },
-      include: { service: true, barbershop: true },
-    }),
-    db.booking.findMany({
-      where: {
-        userId: (session.user as any).id,
-        date: { lt: new Date() },
-      },
-      include: { service: true, barbershop: true },
-    }),
+  const [confirmedBookings, concludedBookings] = await Promise.all([
+    getConfirmedBookings(),
+    getConcludedBookings(),
   ]);
 
   return (
-    <div>
+    <>
       <Header />
-
-      <div className="px-5 py-6">
-        <h1 className="mb-6 text-xl font-bold">Agendamentos</h1>
+      <div className="space-y-3 p-5">
+        <h1 className="text-xl font-bold">Agendamentos</h1>
+        {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
+          <p className="text-gray-400">Você não tem agendamentos.</p>
+        )}
 
         {confirmedBookings.length > 0 && (
           <>
-            <h2 className="mb-3 text-sm font-bold uppercase text-gray-400">
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
               Confirmados
             </h2>
-
-            <div className="flex flex-col gap-3">
-              {confirmedBookings.map((booking) => (
-                <BookingItem key={booking.id} bookings={booking} />
-              ))}
-            </div>
+            {confirmedBookings.map((booking) => (
+              <BookingItem
+                key={booking.id}
+                booking={JSON.parse(JSON.stringify(booking))}
+              />
+            ))}
           </>
         )}
 
-        {finishedBookings.length > 0 && (
+        {concludedBookings.length > 0 && (
           <>
-            <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
               Finalizados
             </h2>
-
-            <div className="flex flex-col gap-3">
-              {finishedBookings.map((booking) => (
-                <BookingItem key={booking.id} bookings={booking} />
-              ))}
-            </div>
+            {concludedBookings.map((booking) => (
+              <BookingItem
+                key={booking.id}
+                booking={JSON.parse(JSON.stringify(booking))}
+              />
+            ))}
           </>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
